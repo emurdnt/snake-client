fd113ad341d55282a2e3e7c8cb5dfe286132a224
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-29 16:23:14 +0000

    modified play.js to contain only a require

--- play.js
+++ play.js
@@ -1,22 +1,22 @@
-const net = require('net');
+
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
+// const connect = function() {
+//   const conn = net.createConnection({ 
+//     host: 'localhost',
+//     port: 50541
+//   });
 
-  conn.on('close',(e)=>{
-    console.log("you ded cuz you idled");
-  })
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+//   conn.on('close',(e)=>{
+//     console.log("you ded cuz you idled");
+//   })
+//   // interpret incoming data as text
+//   conn.setEncoding('utf8'); 
 
-  return conn;
-}
-
+//   return conn;
+// }
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();

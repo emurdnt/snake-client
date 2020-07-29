5973f41601c00a6a76488eb9cbfecec34d59ca49
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-29 17:35:03 +0000

    added the code to send my name to the server

--- client.js
+++ client.js
@@ -5,6 +5,10 @@ const connect = function() {
     port: 50541
   });
 
+  conn.on(('connect'),()=>{
+    // console.log("you're in.")
+    conn.write("Name: ECU");
+  });
   conn.on('close',(e)=>{
     console.log("you ded cuz you idled");
   })

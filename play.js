e6289f8ba3aead5416ddba9f570b8181e28223d2
Em Urdaneta <em.urdaneta94@gmail.com>
2020-07-29 20:56:51 +0000

    moved the setupInput and handleUserInput functions to input.js

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
 
 
-/**
- * Establishes connection with the game server
- */
-// const connect = function() {
-//   const conn = net.createConnection({ 
-//     host: 'localhost',
-//     port: 50541
-//   });
-
-//   conn.on('close',(e)=>{
-//     console.log("you ded cuz you idled");
-//   })
-//   // interpret incoming data as text
-//   conn.setEncoding('utf8'); 
-
-//   return conn;
-// }
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
-
-
+setupInput();
 // host: '135.23.222.131',
 // port:  50541
